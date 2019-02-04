e54e1fe8f987f2e395516615eb8202bc3800b549
robynpartridge <robynpartridge@gmail.com>
2019-02-04 15:04:52 +0100

    SQL fix

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,15 @@
 create table products (
-  `id` int(10) unsigned not null AUTO_INCREMENT,
+  `id` int(10) unsigned not null AUTO_INCREMENT primary key,
   serial_nbr varchar(50),
   dop date,
-  status bit
+  status bit,
+  person_id int,
+  foreign key (person_id) references person(id)
 );
 
 create table person (
-  `id` int(10) unsigned not null AUTO_INCREMENT,
-  name varchar(50)
+  `id` int(10) unsigned not null AUTO_INCREMENT primary key,
+  name varchar(50),
+  product_id int,
+  foreign key (product_id) references products(id)
 );
