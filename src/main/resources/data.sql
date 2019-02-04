f407130d8675078ab43dab1912925a91354f82ec
robynpartridge <robynpartridge@gmail.com>
2019-02-04 19:21:34 +0100

    Updated SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ insert into products (serial_nbr, dop, status) values ('IJK987', '2019-01-31', '
 insert into products (serial_nbr, dop, status) values ('ABC123', '2019-02-02', '0');
 
 insert into person (name, product_id) values ('robyn', 1);
-insert into person (name) values ('wendela');
+insert into person (name, product_id) values ('wendela', 2);
 insert into person (name) values ('paul');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,10 @@
+create table person (
+  `id` int(10) unsigned not null AUTO_INCREMENT,
+  name varchar(50),
+  product_id int,
+  constraint pk_person primary key(id)
+);
+
 create table products (
   `id` int(10) unsigned not null AUTO_INCREMENT,
   serial_nbr varchar(50),
@@ -6,12 +13,4 @@ create table products (
   person_id int,
   constraint pk_products primary key(id),
   constraint fk_products foreign key(person_id) references person(id)
-);
-
-create table person (
-  `id` int(10) unsigned not null AUTO_INCREMENT,
-  name varchar(50),
-  product_id int,
-  constraint pk_person primary key(id),
-  constraint fk_person foreign key(product_id) references products(id)
 );
