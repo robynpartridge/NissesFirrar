f5a799123c1ea717e0910c1d727e86b3d81f8d9e
Wendelazz <47218336+Wendelazz@users.noreply.github.com>
2019-02-04 14:50:03 +0100

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,10 +2,10 @@ create table products (
   `id` int(10) unsigned not null AUTO_INCREMENT,
   serial_nbr varchar(50),
   dop date,
-  status bit,
+  status bit
 );
 
-create table person {
+create table person (
   `id` int(10) unsigned not null AUTO_INCREMENT,
-  name varchar(50),
-};
+  name varchar(50)
+);
